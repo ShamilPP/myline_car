10032789c116eaf695fbadf16473f6f0d75ef69f
Shamil <shamilpp4115@gmail.com>
2024-04-25 15:45:53 +0530

    Updated profile screen UI

--- lib/view/screens/profile/profile_screen.dart
+++ lib/view/screens/profile/profile_screen.dart
@@ -1,170 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:myline_car/view/screens/profile/widgets/profile_field.dart';
+import 'package:myline_car/utils/colors.dart';
 import 'package:provider/provider.dart';
 
-import '../../../model/user.dart';
 import '../../../view_model/user_provider.dart';
 import '../splash/splash_screen.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
-
-  @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-  bool showLoading = false;
-  TextEditingController txtPhoneCtrl = TextEditingController();
-  TextEditingController txtNameCtrl = TextEditingController();
-
-  @override
-  void initState() {
-    var provider = Provider.of<UserProvider>(context, listen: false);
-    if (provider.user != null) {
-      txtNameCtrl.text = provider.user!.name;
-      txtPhoneCtrl.text = provider.user!.phone;
-    }
-    super.initState();
-  }
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserProvider>(builder: (context, provider, child) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Profile'),
-          actions: [
-            if (provider.user != null)
-              IconButton(
-                  onPressed: () {
-                    logoutDialog(context);
-                  },
-                  icon: const Icon(Icons.power_settings_new))
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8, top: 8),
-                child: Row(
-                  //Label and Text
-                  children: <Widget>[
-                    //Label
-                    const Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 7, top: 5, left: 7),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text('Name',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                                fontSize: 20,
-                              )),
+    var user = Provider.of<UserProvider>(context, listen: false).user;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile")),
+      body: Column(
+        children: [
+          // User profile photo
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Center(
+              child: Stack(
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    size: 130,
+                    color: Colors.grey,
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    right: 10,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: colors.primaryColor,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-
-                    //TextField
-                    Expanded(
-                        flex: 3,
-                        child: ProfileField(
-                          height: 50,
-                          myController: txtNameCtrl,
-                          isMultiline: false,
-                          padding: 5,
-                        ))
-                  ],
-                ),
-              ),
-
-              //Phone
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8, top: 8),
-                child: Row(
-                  //Label and Text
-                  children: <Widget>[
-                    //Label
-                    const Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.only(right: 7, top: 5, left: 7),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Text('Phone',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                                fontSize: 20,
-                              )),
-                        ),
+                      child: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
                       ),
                     ),
-
-                    //TextField
-                    Expanded(
-                        flex: 3,
-                        child: ProfileField(
-                          enabled: provider.user == null,
-                          height: 50,
-                          inputType: TextInputType.phone,
-                          myController: txtPhoneCtrl,
-                          isMultiline: false,
-                          padding: 5,
-                        ))
-                  ],
-                ),
+                  ),
+                ],
               ),
-              //Save Button
-              if (!showLoading)
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(15),
-                        ),
-                        onPressed: saveUser,
-                        child: Text((provider.user == null) ? 'Save' : 'Update'),
-                      )),
-                )
-              else
-                const Align(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 8),
-                      child: CircularProgressIndicator(),
-                    ))
-            ],
+            ),
           ),
-        ),
-      );
-    });
+
+          // User details
+          _profileTile(user!.name, 'Name'),
+          _profileTile('+91 ${user.phone}', 'Phone number'),
+
+          const Spacer(),
+          // Logout button
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 10),
+            child: SizedBox(
+              width: 140,
+              height: 45,
+              child: TextButton(
+                style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
+                onPressed: () {
+                  logoutDialog(context);
+                },
+                child: Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 20, color: colors.primaryColor),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  void saveUser() async {
-    setState(() {
-      showLoading = true;
-    });
-    if (txtNameCtrl.text.isNotEmpty && txtPhoneCtrl.text.isNotEmpty) {
-      User user = User(name: txtNameCtrl.text, phone: txtPhoneCtrl.text);
-      var provider = Provider.of<UserProvider>(context, listen: false);
-      if (provider.user == null) {
-        // await Provider.of<UserProvider>(context, listen: false).saveUser(user);
-      } else {
-        user.id = provider.user?.id;
-        await Provider.of<UserProvider>(context, listen: false).updateUser(user);
-      }
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const SplashScreen()));
-    } else {
-      Fluttertoast.showToast(msg: 'Fill all field');
-    }
+  _profileTile(String text, String subText) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            subText,
+            style: const TextStyle(fontSize: 15, color: Colors.grey),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            text,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const Divider(
+            thickness: 1,
+            height: 10,
+            color: Colors.grey,
+          )
+        ],
+      ),
+    );
   }
 
   void logoutDialog(BuildContext context) {
@@ -173,13 +111,22 @@ class _ProfileScreenState extends State<ProfileScreen> {
       builder: (ctx) => AlertDialog(
         title: const Text('Are you sure?'),
         content: const Text('Do you want to logout'),
+        surfaceTintColor: Colors.white,
         actions: [
           TextButton(
+              style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))),
+              onPressed: () async {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel')),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)), backgroundColor: colors.primaryColor, foregroundColor: Colors.white),
               onPressed: () async {
                 await Provider.of<UserProvider>(context, listen: false).removeUser();
                 Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (_) => const SplashScreen()), (Route<dynamic> route) => false);
               },
-              child: const Text('Yes'))
+              child: const Text('Logout'))
         ],
       ),
     );
